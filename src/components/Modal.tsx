2d0e282e22351fa28a5f3225fb3dad63300e1a3c
oleg2713 <olegtestgit@gmail.com>
2025-05-21 00:15:29 +0300

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,43 @@
-type ModalProps = {
+'use client'
+import { useState } from "react";
+import { useDebounce } from "@/hooks/useDebaunce";
+import SearchInput from "./SearchInput";
+
+type SearchModalProps<T> = {
+  list: T[];
+  filterFn: (item: T, search: string) => boolean;
+  renderItem: (item: T, onSelect: (item: T) => void) => React.ReactNode;
+  onSelect: (item: T) => void;
   toggleModal: () => void;
-  children: React.ReactNode;
-  styles?: string;
+  className?: string;
+  listClassName: string,
+  listItemClassName: string
 };
 
-export default function Modal({ toggleModal, children, styles = "" }: ModalProps): React.JSX.Element {
+export default function SearchModal<T>({
+  list,
+  filterFn,
+  renderItem,
+  onSelect,
+  toggleModal,
+  className = "",
+  listClassName,
+  listItemClassName
+}: SearchModalProps<T>): React.JSX.Element {
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value, 300);
+
+  const filteredList = list.filter(item => filterFn(item, debouncedValue));
+
   return (
-    <div className={`fixed z-50 ${styles}`}>
-      <div className={styles}>
-        {children}
-      </div>
+    <div className={className}>
+      <SearchInput value={value} setValue={setValue} toggleModal={toggleModal} />
+      <ul className={listClassName}>
+        {filteredList.length === 0 && (
+          <li className={listItemClassName}>No matches found</li>
+        )}
+        {filteredList.map(item => renderItem(item, onSelect))}
+      </ul>
     </div>
   );
 }
